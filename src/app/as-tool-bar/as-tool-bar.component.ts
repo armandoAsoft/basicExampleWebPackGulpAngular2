4220d9f33f9aea9572050ef7a842f7d0646ad76a
armando.sierralta <armando.sierralta@truextend.com>
2017-09-29 18:10:07 -0400

    angular 2 init

---
+++ src/app/as-tool-bar/as-tool-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-as-tool-bar',
+  templateUrl: './as-tool-bar.component.html',
+  styleUrls: ['./as-tool-bar.component.css']
+})
+export class AsToolBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
